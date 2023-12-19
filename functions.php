84e3d0a81d0b78f66cd821489f132b5dc7318d3b
Gareth Gillman <gareth.gillman@brace.co.uk>
2023-12-19 09:36:18 +0000

    add relevanssi code

--- functions.php
+++ functions.php
@@ -32,3 +32,11 @@ function fix_nav_menu_in_search($query)
 	return $query;
 }
 add_filter('pre_get_posts', 'fix_nav_menu_in_search');
+
+function disable_relevanssi_frontend() {
+	if( !is_admin() ) {
+	  remove_filter( 'posts_request', 'relevanssi_prevent_default_request' );
+	  remove_filter( 'posts_pre_query', 'relevanssi_query', 99 );
+	}
+  }
+  add_action('wp_head', 'disable_relevanssi_frontend');
